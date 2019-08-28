63c01f36017124c19cd9d72e8c8ae247ba83b0d0
pinalto <pinoaltotm@gmail.com>
2019-08-28 12:29:34 +0900

    fix favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,12 +22,12 @@ export default {
       { name: 'theme-color', content: '#008000' }
     ],
     link: [
-      { rel: 'apple-touch-icon', size: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'icon', type: 'image/png', size: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', size: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
+      { rel: 'apple-touch-icon', size: '180x180', href: 'apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
+      { rel: 'icon', type: 'image/png', size: '32x32', href: 'favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', size: '16x16', href: 'favicon-16x16.png' },
+      { rel: 'manifest', href: 'site.webmanifest' },
+      { rel: 'mask-icon', href: 'safari-pinned-tab.svg', color: '#5bbad5' }
     ]
   },
 
