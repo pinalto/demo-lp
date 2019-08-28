aaaf56132bcee5d5c406e029c50178efd216cc03
pinalto <pinoaltotm@gmail.com>
2019-08-28 11:48:36 +0900

    1st

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="root_wrapper" class="root_wrapper">
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.root_wrapper {
+  box-shadow: 0 0 18px green;
+  margin: auto;
+  @include pc {
+    max-width: 1366px;
+  }
+  position: relative;
+}
+</style>
